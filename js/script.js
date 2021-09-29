f9c2d7d53b8f1ecc4dda19eba6b1da478da7635d
Muhammad Mohib <mohibejaz111111@gmail.com>
2021-09-29 22:12:32 +0500

    More questions added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const talkBtn = document.getElementById("talkBtn");
 const content = document.getElementById("content");
 
-const tasks = ["I am opening YouTube"];
+const about = ["I am a voice assistant","I am a good voice assistant"];
+const howIAm = ["I am good","I am quite good","I am cool"];
+const likeMe = ["I have a girlfriend","I hate you","Yes"];
 
 const speechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -22,6 +24,7 @@ recognition.onresult = (e) => {
 
 talkBtn.addEventListener("click", () => {
   recognition.start();
+  console.log("Clicked!")
 });
 
 const readOut = (message) => {
@@ -29,11 +32,38 @@ const readOut = (message) => {
 
   speech.text = "I am not able to understand";
 
-  if (message.includes("YouTube")) {
-    const finalText = tasks[Math.floor(Math.random()=tasks.length)];
+  if (message.includes('who are you')) {
+    const finalText = about[Math.floor(Math.random()*about.length)];
 
     speech.text = finalText;
+    // window.open("https://youtube.com")
+  }
+  else if (message.includes('open YouTube')) {
+    speech.text = "Opening Youtube";
     window.open("https://youtube.com")
+  }
+  else if (message.includes('open Facebook')) {
+    speech.text = "Opening Facebook";
+    window.open("https://facebook.com")
+  }
+  else if (message.includes('open Google')) {
+    speech.text = "Opening Google";
+    window.open("https://google.com")
+  }
+  else if (message.includes('how are you')) {
+    const finalText = howIAm[Math.floor(Math.random()*howIAm.length)];
+
+    speech.text = finalText;
+  }
+  else if (message.includes('do you like me')) {
+    const finalText = likeMe[Math.floor(Math.random()*likeMe.length)];
+
+    speech.text = finalText;
+  }
+  else if (message.includes('what is the time')) {
+    const finalText = new Date().toTimeString().split(" ")[0];;
+
+    speech.text = finalText;
   }
 
   speech.volume = 1;
